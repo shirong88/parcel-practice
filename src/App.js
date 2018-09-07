84f84526e2fd7d4fdbb494e18b42febed3970f28
shirong <sykim2@parkingcloud.co.kr>
2018-09-07 16:55:49 +0900

    작업 완료

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
 import React from 'react';
+import styles from './styles.scss';
 
-export default () => <div>Hello world</div>
+export default () => <div className={styles.title}><h1>Hello!</h1><p>sroan jin</p></div>
